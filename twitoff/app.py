b0a119ed86f3f9c2e9f1ae39d87dc52b29f21950
kevmanning <kevmanning@protonmail.com>
2021-08-10 13:31:36 -0400

    Project at end of sprint 1

--- twitoff/app.py
+++ twitoff/app.py
@@ -2,15 +2,21 @@
 """Main app/routing file for Twitoff"""
 
 from flask import Flask, render_template
+from .models import DB, User
 
 
 def create_app():
     """Creating and configuring an instance of the Flask application"""
     app = Flask(__name__)
-    
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+    app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+
+    DB.init_app(app)
+
     @app.route('/')
     def root():
-        return render_template('base.html', title='home')
+        return render_template('base.html', title="home", users=User.query.all())
 
-    
+
     return app
